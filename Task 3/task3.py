7c8ce50c72b2beaf8d45001fa40511037bc088c7
Beela <bla@senia.dk>
2022-05-05 11:45:47 +0200

    trying some stuff

--- Task 3/task3.py
+++ Task 3/task3.py
@@ -12,10 +12,11 @@ dfSmall = df[["redAvgLevel","blueAvgLevel","redChampKills","blueChampKills","blu
 #sns.boxplot(x=dfSmall["blue_win"], y=dfSmall["redChampKills"])
 #sns.boxplot(x=dfSmall["blue_win"], y=dfSmall["blueChampKills"])
 
-dfTest = pd.DataFrame(data=dfSmall, id_vars=["blue_win"], columns=["redAvgLevel","blueAvgLevel"])
+dfTest = pd.DataFrame(data=dfSmall, columns=["redAvgLevel","blueAvgLevel", "blue_win"])
 
-print(dfTest)
-
-sns.boxplot(x=dfTest["blue_win"], y="value", data=df.melt(dfTest))
-
+#print(dfTest)
+data=dfTest.melt(dfTest)
+#print(data)
+#sns.boxplot(x=dfTest["blue_win"], y="value", data=data)
+sns.boxplot(data=dfTest)
 plt.show()
