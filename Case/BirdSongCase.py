af0d1ffc1f9001c3a802aeab9dd4279405d0f8d3
Beela <bla@senia.dk>
2022-05-12 11:02:32 +0200

    tried some things

--- Case/BirdSongCase.py
+++ Case/BirdSongCase.py
@@ -6,25 +6,36 @@ from sklearn.decomposition import NMF
 import time as t
 
 start_time = t.time()
-data, samplerate = sf.read("../Case/songs/xc26789.flac")
+data, samplerate = sf.read("../Case/songs/xc160879.flac")
 freq, time, Sxx = signal.spectrogram(data, samplerate, scaling='spectrum')
 print("--- %s seconds ---" % (t.time() - start_time))
+print(Sxx.shape)
 
-Pxx, freqs, bins, im = plt.specgram(data, Fs=samplerate)
+#print(Sxx)
+model = NMF(n_components=10,init='random', random_state=0,max_iter=1)
+#print(model)
+W = model.fit_transform(Sxx)
+#print(W)
+print(W.shape)
+print(model.components_.shape)
+H = model.components_
+#print(model.components_)
+print("--- %s seconds ---" % (t.time() - start_time))
+
+fig1, ax1 = plt.subplots(1,1)
+fig2, ax2 = plt.subplots(1,1)
+
+ax1.matshow(Sxx)
+
 plt.ylabel('Frequency [Hz]')
 plt.xlabel('Time [sec]')
 
 print("--- %s seconds ---" % (t.time() - start_time))
 
-#print(Sxx)
-model = NMF(n_components=10,init='random', random_state=0,max_iter=200)
-#print(model)
-W = model.fit_transform(Sxx)
-print(W)
-print(model.components_)
-print("--- %s seconds ---" % (t.time() - start_time))
+estimatedSpectrogram = np.dot(W,H)
+print(estimatedSpectrogram.shape)
 
-
+ax2.matshow(estimatedSpectrogram)
 
 plt.show()
 
